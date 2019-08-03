49aae937e3f7cd2585cb34d07c3e21fb94928bab
xiaiyu <1141600195@qq.com>
2019-08-03 11:49:03 +0800

    标题修改

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import shouye from '@/view/shouye/shouye'
+
+import login  from '@/view/login/login'
+
+
+Vue.use(Router)
+
+export default new Router({
+  mode:'history',
+  routes: [
+    {
+      path: '/view/shouye/shouye',
+      name: 'shouye',
+      component: shouye,
+      meta:{
+        require:true//该路径的访问需要登录
+      },
+      children:[
+
+      ]
+    },{
+      path: '/',
+      name: 'login',
+      component: login,
+      meta:{
+        require:false//该路径的访问不需要登录
+      }
+    }/*,{
+      path: '/error',
+      name: 'error',
+      component: error,
+      meta:{
+        require:false//该路径的访问不需要登录
+      }
+    }*/
+  ]
+})
---
+++ src/view/shouye/shouye.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="width:100%;height: 100%;">
+   111111111111111111111111
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
